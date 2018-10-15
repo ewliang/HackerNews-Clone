7a8ce4820653b9e0d8f9bc002597cdf5c4859f44
DESKTOP-DNMRE55\Eric Liang <ericwliang94@gmail.com>
2018-10-15 08:33:04 -0700

    Refactored index.html from /src/ to /public/ folder. Installed react-router library.

---
+++ src/components/MainHeaderComponent/MainHeaderComponent.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class MainHeaderComponent extends React.Component {
+  render() {
+    return (
+      <header>
+        <img src = "" alt = ""/>
+        <nav>
+          <ul>
+            <li><a href = "">New</a></li>
+            <li><a href = "">Comments</a></li>
+            <li><a href = "">Show</a></li>
+            <li><a href = "">Ask</a></li>
+            <li><a href = "">Jobs</a></li>
+          </ul>
+        </nav>
+      </header>
+    );
+  }
+}
+
+export default MainHeaderComponent;
