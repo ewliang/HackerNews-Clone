5b37ae41ef890d9d10c849a56bc85c3fff1f9b4e
DESKTOP-DNMRE55\Eric Liang <ericwliang94@gmail.com>
2018-10-14 23:01:33 -0700

    Added all dependencies and fixed all errors necessary for Webpack to work for React.

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,27 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/app.js',
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.js$/,
-        exclude: /(node_modules)/,
+        exclude: /node_modules/,
         loader: 'babel-loader',
-        query: {
-          presets: ['es2015']
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react']
         }
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
 };
