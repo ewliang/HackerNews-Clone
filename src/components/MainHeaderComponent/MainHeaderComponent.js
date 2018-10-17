9060cec111f11a12bd39cd4ca36d3c051950795a
DESKTOP-DNMRE55\Eric Liang <ericwliang94@gmail.com>
2018-10-16 17:44:18 -0700

    Refactored navbar to separate component. Logo import still not working...

--- src/components/MainHeaderComponent/MainHeaderComponent.js
+++ src/components/MainHeaderComponent/MainHeaderComponent.js
@@ -1,20 +1,13 @@
 import React, { Component } from 'react';
 import styles from './MainHeaderComponent.css';
+import Logo from '../../../public/imgs/logo.png';
 
 class MainHeaderComponent extends Component {
   render() {
     return (
       <header className = { styles['main-header'] }>
-        <img src = "" alt = "HackerNews Clone Logo"/>
-        <nav>
-          <ul>
-            <li><a href = "">New</a></li>
-            <li><a href = "">Comments</a></li>
-            <li><a href = "">Show</a></li>
-            <li><a href = "">Ask</a></li>
-            <li><a href = "">Jobs</a></li>
-          </ul>
-        </nav>
+        <img src = { Logo } alt = "HackerNews Clone Logo"/>
+        <span className = { styles.sitename }>HackerNews</span>
       </header>
     );
   }
