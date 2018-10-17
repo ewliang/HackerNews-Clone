51668511e56b9d7c54aa90827ec1aaa5179a9faf
DESKTOP-DNMRE55\Eric Liang <ericwliang94@gmail.com>
2018-10-16 17:43:01 -0700

    Added navbar component.

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import MainHeaderComponent from './MainHeaderComponent/MainHeaderComponent.js';
+import MainNavbarComponent from './MainNavbarComponent/MainNavbarComponent.js';
 
 class App extends Component {
   render() {
     return (
       <div>
         <MainHeaderComponent/>
+        <MainNavbarComponent/>
       </div>
     );
   }
