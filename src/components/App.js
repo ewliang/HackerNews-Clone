ac4c863bfd6a0597b197df0fb904a2dc2a3a5207
DESKTOP-DNMRE55\Eric Liang <ericwliang94@gmail.com>
2018-10-16 22:45:20 -0700

    Added BrowserRouter logic for supporting SPA routes in React.js from react-router-dom library.

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import MainHeaderComponent from './MainHeaderComponent/MainHeaderComponent.js';
 import MainNavbarComponent from './MainNavbarComponent/MainNavbarComponent.js';
+
+import HomePageComponent from './HomePageComponent/HomePageComponent.js';
+import NewPageComponent from './NewPageComponent/NewPageComponent.js';
+import CommentsPageComponent from './CommentsPageComponent/CommentsPageComponent.js';
+import ShowPageComponent from './ShowPageComponent/ShowPageComponent.js';
+import AskPageComponent from './AskPageComponent/AskPageComponent.js';
+import JobsPageComponent from './JobsPageComponent/JobsPageComponent.js';
+import ErrorPageComponent from './ErrorPageComponent/ErrorPageComponent.js';
+
 
 class App extends Component {
   render() {
@@ -8,6 +18,17 @@ class App extends Component {
       <div>
         <MainHeaderComponent/>
         <MainNavbarComponent/>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path = '/' component = { HomePageComponent } />
+            <Route exact path = '/newest' component = { NewPageComponent } />
+            <Route exact path = '/comments' component = { CommentsPageComponent } />
+            <Route exact path = '/show' component = { ShowPageComponent } />
+            <Route exact path = '/ask' component = { AskPageComponent } />
+            <Route exact path = '/jobs' component = { JobsPageComponent } />
+            <Route component = { ErrorPageComponent } />
+          </Switch>
+        </BrowserRouter>
       </div>
     );
   }
