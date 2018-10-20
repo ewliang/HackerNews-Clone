4a9b48becc2580534bcb2b46da01cd6e860cc17a
DESKTOP-DNMRE55\Eric Liang <ericwliang94@gmail.com>
2018-10-19 22:16:43 -0700

    Added babel-polyfill

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 // Webpack 4
 module.exports = {
-  entry: './src/index.js',
+  entry: ['babel-polyfill', './src/index.js'],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
