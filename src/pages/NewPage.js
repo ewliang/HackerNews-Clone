7b030203306474c696a5475fed96fdd7a632724b
DESKTOP-DNMRE55\Eric Liang <ericwliang94@gmail.com>
2018-10-20 22:20:56 -0700

    Parentheses formatting update.

--- src/pages/NewPage.js
+++ src/pages/NewPage.js
@@ -60,7 +60,7 @@ class NewPageComponent extends Component {
           {
             posts.map((post) => (
               <li key = { post.id }>
-                <a href = { post.url } className = { styles['post-list-title'] }>{ post.title }</a> <small><a href = { post.url }>({ (post.hasOwnProperty('url')) ? this.parseRootURL(post.url) : post.url })</a></small>
+                <a href = { post.url } className = { styles['post-list-title'] }>{ post.title }</a> <small><a href = { post.url }>{ (post.hasOwnProperty('url')) ? '(' + this.parseRootURL(post.url) + ')' : '' }</a></small>
                 <br/>
                 <small>{ (post.score > 1) ? post.score + ' points': post.score + ' point' } by <a href = { `/user/${post.by}` }>{ post.by }</a> | { post.time } | <a href = { `/item/${post.id}` }>{ (post.kids) ? ((post.kids.length > 1) ? (post.kids.length + ' comments') : '1 comment') : '0 comments' }</a></small>
               </li>
